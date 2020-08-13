aaeb6b77e0356841ddf96b853b963ee66b4d081b
Purav Datta <puravdatta@gmail.com>
2020-08-12 20:38:58 -0500

    setup pycharm

--- venv/include/main.py
+++ venv/include/main.py
@@ -1,1 +1,30 @@
-something in the file
+
+from O365 import *
+
+# User's credentials
+e = 'purav@electrorobotics.onmicrosoft.com'
+p = 'Purav2005'
+
+# Create Schedule object, get calendars, and create empty dict
+schedule = Schedule((e, p))
+result = schedule.getCalendars()
+
+# Funciton to invoke getName() and bind to var result
+def cal_name():
+    result = cal.getName() # This will get the name of a calendar
+    return result
+
+# Show name of each calendar
+print('\nHere are your calendars:\n')
+for cal in schedule.calendars:
+    print(cal_name())
+
+# Get events for each calendar
+print('\nHere are all your upcoming events:\n')
+for cal in schedule.calendars:
+    result = cal_name()
+    events = cal.getEvents() # This will create an Event object
+    for x in cal.events:
+        contents = x.getSubject() # This will get the subject
+        date = x.getStart() # This will get the start time and we'll parse it below
+        print('{}-{} | {} : {}'.format(date.tm_mon, date.tm_mday, result, contents))
